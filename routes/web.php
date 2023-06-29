6d01d63259b5cc54916b7eacca9edeffa16533f8
unknown <mohammadshamsnora@gmail.com>
2023-06-29 22:32:14 +0330

    first update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home');
+});
+Route::get('/admin1', function () {
+    return view('admin.home');
 });
